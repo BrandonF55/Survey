a0865e2c6938434492239934aabcb58a49326aec
brandon fenk <brandon.fenk99@gmail.com>
2023-02-20 11:08:59 -0800

    adding the rest of the files to the project co-authored-by: Lucas Mollerstuen <mollerstuen.works@gmail.com>

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from 'react'
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
